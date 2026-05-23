4401bd5a44ee0179164ea5832f004f71616215c2
vijayvenkatj <vijayvenkatj@gmail.com>
2026-05-23 13:30:19 +0530

    feat(job): added job factory

--- internal/core/job.go
+++ internal/core/job.go
@@ -3,6 +3,7 @@ package core
 import (
 	"net/http"
 	"net/url"
+	"sync/atomic"
 )
 
 type Job struct {
@@ -14,4 +15,25 @@ type Job struct {
 	Body   []byte
 
 	Headers http.Header
+}
+
+func CreateJobFactory(urlStr string, method string, body []byte, headers http.Header) func() Job {
+
+	url, err := url.Parse(urlStr)
+	if err != nil {
+		return nil
+	}
+
+	return func() Job {
+		var id uint32
+		return Job{
+			ID:  atomic.AddUint32(&id, 1),
+			URL: url,
+
+			Method: method,
+			Body:   body,
+
+			Headers: headers,
+		}
+	}
 }
